98ef515816f809f85980c8ee109ed4666d18e7b6
ctrlvee <me123opportunity@gmail.com>
2021-06-27 13:18:32 -0400

    Createsquares function

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const gridContainer = document.querySelector(".gridContainer");
 gridContainer.style.cssText = 'display: block; background-color: grey';
 
+function createSquares() {
 
-for (let i=1;i<=16;i++) {
-    let squareCount = 0;
+    for (let i=1;i<=16;i++) {
+        let squareCount = 0;
 
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add(`rowDiv${i}`);
-    rowDiv.style.cssText = 'display: flex';
-    gridContainer.appendChild(rowDiv);
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add(`rowDiv${i}`);
+        rowDiv.style.cssText = 'display: flex';
+        gridContainer.appendChild(rowDiv);
 
-    for (let j=1; j<=16; j++) {
-    squareCount += 1
-    const squareGrid = document.createElement('div');
-    squareGrid.style.cssText = 'height: 50px; width: 50px; background-color: blue; border-style: dotted;';
-    squareGrid.classList.add(`square${squareCount}`);
-    squareGrid.textContent =`square${squareCount}`;
-    rowDiv.appendChild(squareGrid);
+        for (let j=1; j<=16; j++) {
+        squareCount += 1
+        const squareGrid = document.createElement('div');
+        squareGrid.style.cssText = 'height: 1em; width: 1em; background-color: white; border-style: dotted';
+        squareGrid.classList.add(`square${squareCount}`);
+        squareGrid.textContent =`${squareCount}`;
+        rowDiv.appendChild(squareGrid);
+        }
     }
 }
-
+createSquares();
