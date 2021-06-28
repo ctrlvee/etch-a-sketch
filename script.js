0092e4e7ba3f06b8fba5e248601fb1310d4d1489
ctrlVee <me123opportunity@gmail.com>
2021-06-28 00:46:26 -0400

    Hover effect added

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ gridContainer.style.cssText = 'display: block; background-color: grey';
 function createSquares() {
 
     for (let i=1;i<=16;i++) {
-        let squareCount = 0;
+       
 
         const rowDiv = document.createElement('div');
-        rowDiv.classList.add(`rowDiv${i}`);
+        //rowDiv.classList.add(`rowDiv${i}`);
         rowDiv.style.cssText = 'display: flex';
         gridContainer.appendChild(rowDiv);
 
         for (let j=1; j<=16; j++) {
-        squareCount += 1
         const squareGrid = document.createElement('div');
-        squareGrid.style.cssText = 'height: 1em; width: 1em; background-color: white; border-style: dotted';
-        squareGrid.classList.add(`square${squareCount}`);
-        squareGrid.textContent =`${squareCount}`;
+        squareGrid.style.cssText = 'height: 50px; width: 50px; background-color: white; border-style: dotted';
+        squareGrid.classList.add(`squareGrid`);
+        //squareGrid.textContent =`${squareCount}`;
         rowDiv.appendChild(squareGrid);
         }
     }
 }
+
+
 createSquares();
+
+const squares = Array.from(document.querySelectorAll('.squareGrid'));
+
+squares.forEach(function(square) {
+    square.addEventListener('mouseover', function () {
+        square.style.backgroundColor = 'black';
+        
+    });
+
+    
+});
+
