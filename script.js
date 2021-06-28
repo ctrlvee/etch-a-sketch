8d97309dbae3c172938161c281f8233e37e92a35
ctrlVee <me123opportunity@gmail.com>
2021-06-28 15:50:24 -0400

    User only provides 1 integer and it becomes size of length/width

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const gridContainer = document.querySelector(".gridContainer");
 gridContainer.style.cssText = 'display: block; background-color: grey';
 
 function createSquares() {
-    gridLength = prompt("Enter desired length");
-    gridWidth = prompt("Enter desired width");
+    gridSize = prompt("Enter desired size ranging from 2x2 to 64x64");
+    
 
-    // loop for width
-    for (let i=1;i<=gridWidth;i++) {
+    
+    for (let i=1;i<=gridSize;i++) {
         const rowDiv = document.createElement('div');
         //rowDiv.classList.add(`rowDiv${i}`);
         rowDiv.style.cssText = 'display: flex';
         gridContainer.appendChild(rowDiv);
 
-        // loop for length
-        for (let j=1; j<=gridLength; j++) {
+        
+        for (let j=1; j<=gridSize; j++) {
         const squareGrid = document.createElement('div');
         squareGrid.style.cssText = 'height: 50px; width: 50px; background-color: white; border-style: dotted';
         squareGrid.classList.add(`squareGrid`);
