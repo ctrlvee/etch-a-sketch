cd576cb6af37918f769016cf394e1269e9dc9eb5
ctrlVee <me123opportunity@gmail.com>
2021-06-28 20:55:49 -0400

    Fixing stuff - not working

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 /* Grid and squres */
 const gridContainer = document.querySelector(".gridContainer");
-gridContainer.style.cssText = 'display: block; background-color: grey';
+gridContainer.style.cssText = '';
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+gridContainer.appendChild(grid);
 
 function createSquares() {
     let gridSize = prompt("Enter desired size ranging from 2x2 to 64x64");
     if (gridSize === null) {
          gridSize = 16;
-    };
+    }
     console.log(gridSize);
 
     
-    for (let i=1;i<=gridSize;i++) {
+    for (let i=1;i<=16;i++) {
         const rowDiv = document.createElement('div');
         //rowDiv.classList.add(`rowDiv${i}`);
         rowDiv.style.cssText = 'display: flex';
-        gridContainer.appendChild(rowDiv);
+        rowDiv.classList.add('rowDiv');
+        grid.appendChild(rowDiv);
 
         
-        for (let j=1; j<=gridSize; j++) {
+        for (let j=1; j<=16; j++) {
         const squareGrid = document.createElement('div');
-        squareGrid.style.cssText = 'height: 2px; width: 2px;  background-color: white; border-style: dotted;'
+        squareGrid.style.cssText = 'height: 10px; width: 10px;  background-color: white';
         squareGrid.classList.add(`squareGrid`);
         //squareGrid.textContent =`${squareCount}`;
         rowDiv.appendChild(squareGrid);
@@ -34,6 +39,7 @@ const squares = Array.from(document.querySelectorAll('.squareGrid'));
 squares.forEach(function(square) {
     square.addEventListener('mouseover', function () {
         square.style.backgroundColor = 'black';
+        console.log('works');
     });
 });
 
@@ -41,7 +47,7 @@ squares.forEach(function(square) {
 /* Create container and clear button feature */
 const buttonContainer = document.createElement('div');
 buttonContainer.classList.add('buttonContainer');
-document.body.appendChild(buttonContainer);
+document.body.insertBefore(buttonContainer, gridContainer);
 // Create clear button
 const clearbutton = document.createElement('button');
 clearbutton.classList.add('clearBTN');
