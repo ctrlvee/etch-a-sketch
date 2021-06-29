b7ab221a6633552ba1e751ca660a5a852f060c78
ctrlVee <me123opportunity@gmail.com>
2021-06-29 15:20:27 -0400

    Container and square boxes set on fixed size and only squares inside change proportionaly in size to the containers height/width

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let pen_color = '#000';
 /* Grid and squres */
 const gridContainer = document.querySelector(".gridContainer");
 gridContainer.style.cssText = '';
@@ -14,6 +15,15 @@ function createSquares() {
     console.log(gridSize);
 
     
+    for (let i=1; i<=(gridSize**2); i++) {
+        const squareGrid = document.createElement('div');
+        squareGrid.style.cssText =  `height: ${200/gridSize}px; width:${200/gridSize}px; background-color: white`;
+        squareGrid.classList.add(`squareGrid`);
+        //squareGrid.textContent =`${squareCount}`;
+        grid.appendChild(squareGrid);
+    }
+    
+    /*
     for (let i=1;i<=16;i++) {
         const rowDiv = document.createElement('div');
         //rowDiv.classList.add(`rowDiv${i}`);
@@ -24,12 +34,15 @@ function createSquares() {
         
         for (let j=1; j<=16; j++) {
         const squareGrid = document.createElement('div');
-        squareGrid.style.cssText = 'height: 10px; width: 10px;  background-color: white';
+        squareGrid.style.cssText =  `height: ${gridArea/gridSize}; width:${gridArea/gridSize}px; background-color: white`;
         squareGrid.classList.add(`squareGrid`);
         //squareGrid.textContent =`${squareCount}`;
         rowDiv.appendChild(squareGrid);
         }
     }
+    */
+    
+    
 }
 
 
@@ -38,21 +51,38 @@ createSquares();
 const squares = Array.from(document.querySelectorAll('.squareGrid'));
 squares.forEach(function(square) {
     square.addEventListener('mouseover', function () {
-        square.style.backgroundColor = 'black';
+        square.style.backgroundColor = pen_color;
         console.log('works');
     });
 });
-
+function randomColor () {
+    value = parseInt((Math.random() * 999));
+    return `#${value}`
+}
 
 /* Create container and clear button feature */
 const buttonContainer = document.createElement('div');
 buttonContainer.classList.add('buttonContainer');
 document.body.insertBefore(buttonContainer, gridContainer);
+
+// Random color button
+const randomBtn = document.createElement('button');
+randomBtn.classList.add('randomBtn');
+randomBtn.textContent = 'Random color';
+randomBtn.style.cssText = 'height: 50px; width: 50px;';
+randomBtn.addEventListener('click', function () {
+    pen_color = randomColor();
+    console.log(pen_color);
+});
+buttonContainer.appendChild(randomBtn);
+
+
+
 // Create clear button
 const clearbutton = document.createElement('button');
 clearbutton.classList.add('clearBTN');
 clearbutton.textContent = 'Clear';
-clearbutton.style.cssText = 'height: 100px; width: 50px';
+clearbutton.style.cssText = 'height: 50px; width: 50px';
 buttonContainer.appendChild(clearbutton);
 clearbutton.addEventListener('click', clearBoard);
 function clearBoard () {
